c84ed5b061c35c80b8b951d2955f7efc140414ac
Philip Spahn <philip.spahn@adesso.de>
2018-10-04 12:54:00 +0200

    ändern des scripts.java

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -15,23 +15,29 @@ else
 fi
 
 docker cp ../upload-dir/$1.pdf converter:/root
-
-docker exec converter pdftotext -bbox $1.pdf
-docker exec converter mv /root/$1.html /root/$1_html.txt
-docker cp converter:/root/$1_html.txt ../src/main/resources/generated_files
-docker exec converter abiword --to=tex $1.pdf
-docker exec converter pandoc $1.tex -o $1.txt
-docker cp converter:/root/$1.txt ../src/main/resources/generated_files
-docker exec converter convert -density 300 $1.pdf -fuzz 33%% +opaque "#33cc33" -black-threshold 90%% -fill black -depth 8 -strip -background white -alpha off $1_only_green.tiff
-docker exec converter tesseract -psm 4 -l deu $1_only_green.tiff $1_only_green
-docker cp converter:/root/$1_only_green.txt ../src/main/resources/generated_files
-
-docker exec converter rm $1*
 if [ $? -eq 0 ]; then
-    echo $1 removed from converter
+    echo starting to convert $1
+    docker exec converter pdftotext -bbox $1.pdf
+    docker exec converter mv /root/$1.html /root/$1_html.txt
+    docker cp converter:/root/$1_html.txt ../src/main/resources/generated_files
+    docker exec converter abiword --to=tex $1.pdf
+    docker exec converter pandoc $1.tex -o $1.txt
+    docker cp converter:/root/$1.txt ../src/main/resources/generated_files
+    docker exec converter convert -density 300 $1.pdf -fuzz 33%% +opaque "#33cc33" -black-threshold 90%% -fill black -depth 8 -strip -background white -alpha off $1_only_green.tiff
+    docker exec converter tesseract -psm 4 -l deu $1_only_green.tiff $1_only_green
+    docker cp converter:/root/$1_only_green.txt ../src/main/resources/generated_files
+
+    docker exec converter rm $1*
+    if [ $? -eq 0 ]; then
+        echo $1 removed from converter
+    else
+        echo No $1 files exist on converter
+    fi
 else
-    echo No $1 files exist on converter
+    echo $1 dosent exist
 fi
+
+
 
 
 
