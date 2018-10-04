ba07857042b3c1eb2f9bca6e151069ab3c028403
Philip Spahn <philip.spahn@adesso.de>
2018-10-04 12:34:58 +0200

    change scripts

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm ../src/main/resources/generated_files/$1*
+rm -f ../src/main/resources/generated_files/$1*
 docker exec converter rm $1.tex
 docker exec converter rm $1.pdf
 docker exec converter rm $1.tiff
@@ -8,9 +8,10 @@ docker exec converter rm $1.txt
 docker exec converter rm $1_only_green.txt
 docker exec converter rm $1_only_green.tiff
 
+docker exec converter ls -a
 
 docker cp ../upload-dir/$1.pdf converter:/root
-docker exec converter ls -la
+
 
 
 docker exec converter pdftotext -bbox $1.pdf
@@ -22,6 +23,8 @@ docker cp converter:/root/$1.txt ../src/main/resources/generated_files
 docker exec converter convert -density 300 $1.pdf -fuzz 33%% +opaque "#33cc33" -black-threshold 90%% -fill black -depth 8 -strip -background white -alpha off $1_only_green.tiff
 docker exec converter tesseract -psm 4 -l deu $1_only_green.tiff $1_only_green
 docker cp converter:/root/$1_only_green.txt ../src/main/resources/generated_files
+
+docker exec converter ls -a
 
 docker exec converter rm $1_html.txt
 docker exec converter rm $1.tex
